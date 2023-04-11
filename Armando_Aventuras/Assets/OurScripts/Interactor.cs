f7abae9e29c9b99435ec3d8bdecceaaf53574f2c
MHF13 <41444696+MHF13@users.noreply.github.com>
2023-04-11 21:07:05 +0200

    Interaction (Works)

--- Armando_Aventuras/Assets/OurScripts/Interactor.cs
+++ Armando_Aventuras/Assets/OurScripts/Interactor.cs
@@ -7,6 +7,8 @@ public class Interactor : MonoBehaviour
     [SerializeField] private Vector3 pos;
     [SerializeField] private float radius;
     [SerializeField] private LayerMask layer;
+    private IInteractable interact = null;
+    private int index = 0;
 
 
     [SerializeField] private InteractionCanva canva;
@@ -14,7 +16,8 @@ public class Interactor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        interact = null;
+        index = 0;
     }
 
     // Update is called once per frame
@@ -22,34 +25,45 @@ public class Interactor : MonoBehaviour
     {
        Collider[] coll = Physics.OverlapSphere(this.transform.position + pos, radius);
 
+        bool find = false;
+
         for (int i = 0; i < coll.Length; i++)
         {
             if (coll[i].GetComponent<IInteractable>() != null)
             {
-                IInteractable interact = coll[i].GetComponent<IInteractable>();
+                index = i;
+                find = true;
+            }
+        }
+        if (find)
+        {
+            interact = coll[index].GetComponent<IInteractable>();
 
-                if (!canva.On)
-                {
-                    canva.Open(interact.Text);
-                    if (Accion())
-                    {
-                        interact.Reaction();
-                    }
-                }
-                break;
+            if (!canva.On)
+            {
+                canva.Open(interact.Text);
+                Debug.Log("Algo cerca");
             }
             else
             {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    interact.Reaction();
+                }
+            }
+        }
+        else
+        {
+            if (canva.On)
+            {
+                interact = null;
                 canva.Close();
             }
-
         }
+
+
     }
 
-    private bool Accion()
-    {
-        return Input.GetKeyDown(KeyCode.E);
-    }
 
     private void OnDrawGizmos()
     {
